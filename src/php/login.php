2435e9089a72fb0e29fcde20fc2aa9f429915fe8
alexwin99 <49718581+alexwin99@users.noreply.github.com>
2021-05-25 19:27:06 +0200

    Add files via upload

--- src/php/login.php
+++ src/php/login.php
@@ -3,43 +3,39 @@
     require_once "Connection.php";
     require_once  "Personne.php";
 
-
     $email=$_POST["email"];
     $mdp=$_POST["mdp"];
-    //$statut=$_POST["statut"];
-    $statut="abonne";
+    $statut=$_POST["statut"];
 
     $connection=new Connection();
     $nbRow=0;
 
-
-    if($statut="employe"){
+    if(isset($statut)){
         $connection->initConnectionEmploye();
     }
-    if($statut="abonne"){
+    else{
+        $statut="abonne";
         $connection->initConnectionAbonne();
     }
-    else{
-      echo "Statut ni abonne ni employe";
-    }
-
     $sql=" CALL connection('".$email."','".$mdp."','".$statut."');";
     $result=mysqli_query($connection->conn,$sql);
 
 
-    if(!$result) {
-    echo $connection->conn->error;
-    header('Location: ../html/login.html');
+
+    if(!$result){
+        echo $connection->conn->error;
+        header('Location: ../html/login.html');
+        exit;
     }
-    else {
+    else{
         $row = mysqli_fetch_array($result);
-        $personne = new Personne($row[0], $row[1], $row[2], $row[3], $row[4], $row[5], $row[6], $row[7], $row[8], $row[9], $row[10]);
+        $personne = new Personne($row[0], $row[1], $row[2], $row[3], $row[4], $row[5]);
 
         session_start();
         $_SESSION["compte"]=$personne;
 
-    $connection->closeConnection();
-    header('Location: consulter_catalogue.php');
-  }
+    }
 
-?>
+    $connection->closeConnection();
+    header('Location: monCompte.php');
+    exit;
