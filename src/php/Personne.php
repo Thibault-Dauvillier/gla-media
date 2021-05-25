24e7c3409385cfa5e3b7b2e07a5ee22d3b207d39
alexwin99 <49718581+alexwin99@users.noreply.github.com>
2021-05-25 12:16:10 +0200

    Add files via upload

--- src/php/Personne.php
+++ src/php/Personne.php
@@ -6,29 +6,19 @@ class Personne
     private int $id_personne;
     private string $prenom;
     private string $nom;
-    private string $numero;
-    private string $adresse;
     private string $mail;
-    private string $birthdate;
-    private string $password;
     private string $statut;
     private int $locked;
-    private string $dateFinAbo;
 
-    public function __construct($id_personne,$prenom,$nom,$numero,$adress,$mail,$birthdate,$password,$statut,$locked,$dateFinAbo)
+    public function __construct($id_personne,$nom,$prenom,$mail,$locked,$statut)
     {
 
         $this->id_personne=$id_personne;
         $this->prenom=$prenom;
         $this->nom=$nom;
-        $this->numero=$numero;
-        $this->adresse=$adress;
         $this->mail=$mail;
-        $this->birthdate=$birthdate;
-        $this->password=$password;
         $this->$statut=$statut;
         $this->locked=$locked;
-        $this->dateFinAbo=$dateFinAbo;
 
     }
 
@@ -47,57 +37,17 @@ class Personne
         return $this->prenom;
     }
 
-    /**
-     * @return string
-     */
-    public function getAdresse(): string
-    {
-        return $this->adresse;
-    }
 
-    /**
-     * @return string
-     */
-    public function getBirthdate(): string
-    {
-        return $this->birthdate;
-    }
-
-    /**
-     * @return string
-     */
-    public function getDateFinAbo(): string
-    {
-        return $this->dateFinAbo;
-    }
-
-    /**
-     * @return int
-     */
     public function getLocked(): int
     {
         return $this->locked;
     }
 
-    /**
-     * @return string
-     */
     public function getMail(): string
     {
         return $this->mail;
     }
 
-    /**
-     * @return string
-     */
-    public function getNumero(): string
-    {
-        return $this->numero;
-    }
-
-    /**
-     * @return string
-     */
     public function getStatut(): string
     {
         return $this->statut;
