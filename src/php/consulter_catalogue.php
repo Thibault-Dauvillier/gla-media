2d05e6289f01d2cfd0ff3d032a48e6466bfe7979
gnaitsa <77583253+gnaitsa@users.noreply.github.com>
2021-05-25 19:07:26 +0200

    Update consulter_catalogue.php

--- src/php/consulter_catalogue.php
+++ src/php/consulter_catalogue.php
@@ -3,47 +3,50 @@
   <head>
     <meta charset="utf-8">
     <title>Catalogue</title>
-    <link rel="stylesheet" type="text/css" href="../css/consulter_catalogue.css">
+    <link rel="stylesheet" type="text/css" href="../css/consulter_catalogue.css"> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript">
+
+
+    function Reservation(){
+      alert('Veuillez vous connecter pour pouvoir emprunter nos documents');
+    }
+    </script>
+
   </head>
   <body>
     <nav>
       <ul id="nav">
-  			    <li><a href="index.php"> Accueil</a></li>
-  					<li><a href="a_propos.php">À propos de la médiathèque</a></li>
-  					<li><a href="consulter_catalogue.php">Consulter le catalogue</a></li>
-  					<li><a href="employe.php">Employe</a></li>
-
-      </ul>
+            <li><a href="index.php"> Accueil</a></li>
+            <li><a href="Aporopos.php">À propos de la médiathèque</a></li>
+            <li><a href="consulter_catalogue.php">consulter le catalogue</a></li>
+            <li><a href="aboutOpenHours.php">help</a></li>
+            <li><a href="aboutOpenHours.php">Se connecter</a></li>
+      </ul>	
     </nav>
     <h2>Consulter le Catalogue</h2><br>
-
-
+    
+    <div class= rech>
+    <form action="recherche.php" method="post">
+        <input  type="text" name="nom" id ="nom" placeholder="Search" aria-label="Search"required/>
+        <input  type="submit" value="Recherche Par titre"/>
+        <input  type="submit" value="Recherche Par description "/>
+        <input  type="submit" value="Recherche Par auteur "/>
+      <input  type="submit" value="Recherche Par genre "/></form>
+    
+    <input  type ="submit" value="Recherche Par disponibilité" onclick= "window.location.href ='recherche_par_dispo.php'"/>
+</div>
     <?php
+    require_once "Connection.php";
 
-    //testing connection with user abonne
-    $servername = "localhost";
-    $username = "root"; // root is default user with every privileges
-    $password = ""; // root's default password is "", otherwise the password will be the username of the user
-    $db = "gla_database";
-
-    // Create connection
-    $mysqli = new mysqli($servername,$username,$password,$db);
-
-    // Check connection
-    if ($mysqli -> connect_errno) {
-      echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-      exit();
-    }
-    echo'<form class=" " action="recherche.php" method="POST">';
-    echo'   <input class="" type="text" name="nom" placeholder="Search" aria-label="Search">';
-    echo'<button class="rech"  type="submit">Recherche</button>';
-    echo'  </form>';
+    
+    
     // Perform query
     $query="SELECT * FROM `vue_livre`";
     //prepare display tab
     echo '<table id="produit" border="10" cellspacing="2" cellpadding="2">
       <tr>
-
+          
           <td> <h3>ID</h3></td>
           <td> <h3><font face="Arial">Titres</font></h3> </td>
           <td> <h3><font face="Arial">Description</font> </h3></td>
@@ -52,8 +55,8 @@
           <td><h3> <font face="Arial">Auteur</font></h3> </td>
           <td><h3> <font face="Arial">Genre</font> </h3></td>
           <td> <h3><font face="Arial">Reserver</font></h3> </td>
-
-
+          
+         
       </tr>';
 
     if ($result = $mysqli -> query($query)) {
@@ -75,7 +78,7 @@
                  <td>'.$quantite.'</td>
                  <td>'.$auteur.'</td>
                  <td>'.$genre.'</td>
-                 <td> <a href ="reserver.php"</a>Reserver</td>
+                 <td>  <button id ="des" onclick="Reservation()">Reserver</button></td>
              </tr>';
       }
       // Free result set
